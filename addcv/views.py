b69e5d1559c28b47c55e2e470f889d92c835d109
shreyas2-bit <62927960+shreyas2-bit@users.noreply.github.com>
2020-10-11 19:33:05 +0530

    html to pdf

--- addcv/views.py
+++ addcv/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .forms import personalform, educationform, experienceform, whole
 from .models import experience, education, person
+from django.template.loader import render_to_string
+from weasyprint import HTML
+import tempfile
+from django.http import HttpResponse
+import datetime
 
 
 def personal(request):
@@ -9,7 +14,7 @@ def personal(request):
         #em = educationform(request.POST)
         if fm.is_valid():
             fm.save()
-            content = {
+            _ = {
                 'firstname':fm.cleaned_data['firstname'],
                 'lastname':fm.cleaned_data['lastname'],
                 'profession':fm.cleaned_data['profession'],
@@ -33,7 +38,7 @@ def educational(request):
         if fm.is_valid():
             print("school")
             fm.save()
-            content = {
+            _ = {
                 'institute':fm.cleaned_data['institute'],
                 'startingyear':fm.cleaned_data['startingyear'],
                 'endingyear':fm.cleaned_data['endingyear'],
@@ -79,3 +84,28 @@ def cv(request):
 
 
 # Create your views here.
+
+def export_pdf(request):
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; attachment; filename=Addcv'+\
+        str(datetime.datetime.now())+'.pdf'
+    response['Content-Transfer-Encoding'] = 'binary'
+
+
+    html_string=render_to_string('addcv/pdf-output.html')
+    html=HTML(string=html_string)
+
+    result = html.write_pdf()
+
+
+    with tempfile.NamedTemporaryFile(delete=True)as output:
+        output.write(result)
+        output.flush()
+
+        output=open(output.name,'rb')
+
+        response.write(output.read())
+
+
+    return response
