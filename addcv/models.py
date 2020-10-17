5a19e01f92be8242c8c785a574e3599b71b47947
sreebasu05 <mithi.basu09@gmail.com>
2020-10-17 20:50:30 +0530

    Corrected error

--- addcv/models.py
+++ addcv/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class person(models.Model):
-    firstname=models.CharField(max_length=20)
-    lastname=models.CharField(max_length=20)
-    profession=models.CharField(max_length=20)
-    title=models.CharField(max_length=20)
-    city=models.CharField(max_length=30)
-    state=models.CharField(max_length=30)
-    pincode=models.IntegerField()
-    phoneno=models.IntegerField()
-    emailid=models.EmailField()
-    profile=models.TextField(max_length=100)
+    firstname=models.CharField(max_length=20, null=True)
+    lastname=models.CharField(max_length=20, null=True)
+    profession=models.CharField(max_length=20, null=True)
+    title=models.CharField(max_length=20, null=True)
+    city=models.CharField(max_length=30, null=True)
+    state=models.CharField(max_length=30, null=True)
+    pincode=models.IntegerField( null=True)
+    phoneno=models.IntegerField( null=True)
+    emailid=models.EmailField( null=True)
+    profile=models.TextField(max_length=100, null=True)
 
     def __str__(self):
         return self.firstname
