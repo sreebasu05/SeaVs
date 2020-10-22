e06af4b9adb03a7d53326275cf8e7735b0be2b8a
arushi0106 <arushi.agarwal999@gmail.com>
2020-10-22 16:48:44 +0530

    Modifying models so that every user can create differnt cv

--- addcv/urls.py
+++ addcv/urls.py
@@ -4,15 +4,17 @@ from . import views
 
 urlpatterns = [
      path('personal/', views.personal,name='personal'), #personal form
-     path('education/', views.educational,name='ed'),   #education form
-     path('experiences/', views.experiences,name='ex'), #job form
-     path('project/', views.project,name='project'),    #project form
+     path('education/<int:person_id>', views.educational,name='ed'),   #education form
+     path('experiences/<int:person_id>', views.experiences,name='ex'), #job form
+     path('project/<int:person_id>', views.project,name='project'),    #project form
      path('',views.createcv, name='create-cv-page'),    #createcv page
      #path('resume/', views.resumes, name='resume'),
-     path('myresume/', views.makecv,name='makecv'),      #resume form
+     path('myresume/', views.makecv, name='makecv'),  #resume form
+     path('persondash/<int:person_id>', views.personaldash, name='persondash'), 
+      path('userdash/', views.dashboard,name='dashboard'),
    #   path('madecv/',views.cv,name='cv'),                #cv2
     #  path('export-pdf', views.export_pdf, name='export-pdf'),
-     path('edudashboard/', views.edudashboard, name='edudashboard'),
+     path('edudashboard/<int:test_id>', views.edudashboard, name='edudashboard'),
      path('prodashboard/', views.prodashboard, name='prodashboard'),
      path('jobdashboard/', views.jobdashboard, name='jobdashboard')
  ]
--- addcv/views.py
+++ addcv/views.py
@@ -19,22 +19,24 @@ def personal(request):
             instance = fm.save(commit=False)
             instance.added_by = request.user
             instance.save()
-        return render(request,'addcv/personal.html',{'form':fm})
+            person_id=instance.id
+        return render(request,'addcv/moredetails.html',{'person_id':person_id})
     else:
         fm = personalform()
         #em = educationform()
         return render(request,'addcv/personal.html',{'form':fm})
 
 
-def educational(request):
+def educational(request,person_id):
     if request.method == 'POST':
         fm = educationform(request.POST)
         #em = educationform(request.POST)
         if fm.is_valid():
+            current_user=request.user
+            current_person = person.objects.get(added_by=current_user,id=person_id)
             instance = fm.save(commit=False)
-            instance.added_by = request.user
+            instance.added_by = current_person
             instance.save()
-            fm.save()
 
         return render(request, 'addcv/educational.html', {'form':fm})
     else:
@@ -42,9 +44,11 @@ def educational(request):
         #em = educationform()
         return render(request,'addcv/educational.html',{'form':fm})
 
-def edudashboard(request):
+def edudashboard(request,test_id):
     current_user = request.user
-    content = education.objects.filter(added_by=current_user)
+    current_person = person.objects.get(added_by=current_user,id=test_id)
+    print(current_person)
+    content = education.objects.filter(added_by=current_person)
     return render (request, 'addcv/edudashboard.html',{'content':content})
 
 def prodashboard(request):
@@ -58,28 +62,29 @@ def jobdashboard(request):
     content = experience.objects.filter(added_by=current_user)
     return render (request, 'addcv/jobdashboard.html',{'content':content})
 
-def experiences(request):
+def experiences(request,person_id):
     if request.method == 'POST':
         fm = experienceform(request.POST)
         if fm.is_valid():
             instance = fm.save(commit=False)
-            instance.added_by = request.user
+            current_user=request.user
+            current_person = person.objects.get(added_by=current_user,id=person_id)
+            instance.added_by = current_person
             instance.save()
-            fm.save()
         return render(request,'addcv/experience.html',{'form':fm})
     else:
         fm = experienceform()
-        #em = educationform()
         return render(request,'addcv/experience.html',{'form':fm})
 
-def project(request):
+def project(request,person_id):
     if request.method == 'POST':
         fm = projectform(request.POST)
         if fm.is_valid():
             instance = fm.save(commit=False)
-            instance.added_by = request.user
+            current_user=request.user
+            current_person = person.objects.get(added_by=current_user,id=person_id)
+            instance.added_by = current_person
             instance.save()
-            fm.save()
         return render(request,'addcv/project.html',{'form':fm})
     else:
         fm = projectform()
@@ -95,12 +100,25 @@ def project(request):
 #     }
 #     return render(request, 'resumes/2/index.html', context)
 
-def makecv(request):
+def makecv(request,test_id):
     user = request.user
     print(user)
     ed = user.education_set.all()
-    return render(request,'addcv/personaledit.html',{'op':ed})
+    return render(request, 'addcv/personaledit.html', {'op': ed})
+    
+def dashboard(request):
+    current_user = request.user
+    person1 = person.objects.filter(added_by=current_user)
+    return render(request,'addcv/dashboard.html',{'persons':person1})
 
+def personaldash(request,person_id):
+    current_user = request.user
+    current_person = person.objects.get(added_by=current_user,id=person_id)
+    print(current_person)
+    cont = education.objects.filter(added_by=current_person)     
+    context = experience.objects.filter(added_by=current_person)
+    pro = projects.objects.filter(added_by=current_person)
+    return render (request, 'addcv/persondashboard.html',{'contents':cont,'experiences':context,'projects':pro})
 
 
 # Create your views here.
