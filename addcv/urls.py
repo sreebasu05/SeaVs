99a248f2d1da417ff72183ac2a2d58e6a80e77c1
sreebasu05 <mithi.basu09@gmail.com>
2020-10-22 21:08:05 +0530

    Added updata and delete methods

--- addcv/urls.py
+++ addcv/urls.py
@@ -4,13 +4,19 @@ from . import views
 
 urlpatterns = [
      path('personal/', views.personal,name='personal'), #personal form
+     path('updatepersonal/<int:person_id>', views.updatepersonal,name='updatepersonal'),
+     path('deletepersonal/<int:person_id>', views.deletepersonal,name='deletepersonal'),
+
      path('education/<int:person_id>', views.educational,name='ed'),   #education form
+     path('updateeducation/<int:education_id>', views.updateeducation,name='updateeducation'),
+     path('deleteeducation/<int:education_id>', views.deleteeducation,name='deleteeducation'),
+
      path('experiences/<int:person_id>', views.experiences,name='ex'), #job form
      path('project/<int:person_id>', views.project,name='project'),    #project form
      path('',views.createcv, name='create-cv-page'),    #createcv page
      #path('resume/', views.resumes, name='resume'),
      path('myresume/', views.makecv, name='makecv'),  #resume form
-     path('persondash/<int:person_id>', views.personaldash, name='persondash'), 
+     path('persondash/<int:person_id>', views.personaldash, name='persondash'),
       path('userdash/', views.dashboard,name='dashboard'),
    #   path('madecv/',views.cv,name='cv'),                #cv2
     #  path('export-pdf', views.export_pdf, name='export-pdf'),
